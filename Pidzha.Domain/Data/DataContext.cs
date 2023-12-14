819a77f1aba780e6a7a420801ca2e3159e76ccc0
Wolfram-xx <129691476+Wolfram-xx@users.noreply.github.com>
2023-12-14 20:59:24 +0300

    Add files via upload

---
+++ Pidzha.Domain/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pidzha.Domain.Data
+{
+    internal class DataContext : DbContext
+    {
+        public DbSet<EducationalInstitution> EducationalInstitutions { get; set; } = null!;
+        public DbSet<EducationalPlan> EducationalPlans { get; set; } = null!;
+        public DbSet<Discipline> Disciplines { get; set; } = null!;
+        public DbSet<EducationalProgram> EducationalPrograms { get; set; } = null!;
+        public DbSet<Teacher> Teachers { get; set; } = null!;
+        public DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer();
+        }
+
+    }
+}
